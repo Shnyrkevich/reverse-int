9a0ef04f4a52881e2d60c40fdf5af3df9eee54aa
Евгений Шныркевич <mogrein.dark@gmail.com>
2020-02-21 13:29:25 +0300

    feat: realize reverse-Int

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function reverse (n) {
-  
+  let strNumber = String(n);
+  let rever = "";
+  for(let i = strNumber.length-1; i >= 0; i--){
+      rever += strNumber[i];
+  }
+  return parseInt(rever);
 }
